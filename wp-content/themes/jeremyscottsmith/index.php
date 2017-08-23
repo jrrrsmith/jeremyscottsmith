528b05de0df28a66d63be9c4bc36f2765bd2988b
jrrrsmith <jeremy.scott.smith@me.com>
2017-08-23 11:47:48 -0400

    fonts

--- wp-content/themes/jeremyscottsmith/index.php
+++ wp-content/themes/jeremyscottsmith/index.php
@@ -1,7 +1,10 @@
 <?php get_header();?>
-<div class="search-wrap">
+<div class="wrap">
 <?php while(have_posts()) : the_post(); ?>
   <div class="index-body">
+    <a href="<?php the_permalink(); ?>">
+      <h2><?php the_title(); ?></h2>
+    </a>
     <?php the_content('Read More'); ?>
   </div>
 <?php endwhile; ?>
--- wp-content/themes/jeremyscottsmith/page-templates/partials/head.php
+++ wp-content/themes/jeremyscottsmith/page-templates/partials/head.php
@@ -6,6 +6,7 @@
   <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico?v=2" />
   <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1" >
   <title><?php bloginfo('title'); ?></title>
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans|Work+Sans:300" rel="stylesheet">
   <link rel="stylesheet" href="<?php bloginfo('template_url');?>/assets/css/style.css?<?php echo time(); ?>">
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 </head>
