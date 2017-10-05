4c11556e769e35abac18e4b73d73873fb13bc8b7
jrrrsmith <jeremy.scott.smith@me.com>
2017-10-05 19:49:41 -0400

    updated to newest version of starter theme

--- wp-content/themes/jeremyscottsmith/index.php
+++ wp-content/themes/jeremyscottsmith/index.php
@@ -1,16 +1,8 @@
 <?php get_header();?>
-<div class="wrap">
+<div class="search-wrap">
 <?php while(have_posts()) : the_post(); ?>
-  <div class="blog-item">
-    <a href="<?php the_permalink(); ?>">
-      <h2><?php the_title(); ?></h2>
-      <div class="post-meta-box">
-        <span class="day"><?php the_time('j') ?></span>
-        <span class="month"><?php the_time('F') ?></span>
-        <span class="year"><?php the_time('Y') ?></span>
-      </div>
-      <?php the_content('Read More'); ?>
-    </a>
+  <div class="index-body">
+    <?php the_content('Read More'); ?>
   </div>
 <?php endwhile; ?>
 </div>
